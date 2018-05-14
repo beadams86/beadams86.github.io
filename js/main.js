28fda6df9e282ec818d696a3f79544dd0d76f691
beadams86 <bennett.adams@jspargo.com>
2018-05-14 14:38:20 -0400

    Updated to add EarthScience card, included portfolio pages, starting to figure that out.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
     // main.js
+
+    //Set collapsed state on mobile resolutions
+
+    //var w = window,
+    //d = document,
+    //e = d.documentElement,
+    //g = d.getElementsByTagName('body')[0],
+    //x = w.innerWidth || e.clientWidth || g.clientWidth,
+    //y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+    var toggleBtn = document.querySelector('.toggle-nav');
+    var toggleNav = document.querySelector('.sub-nav-mobile');
+
+    toggleBtn.onclick = function(){
+        toggleNav.classList.toggle("open");
+    }
